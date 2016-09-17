55abda76e4271eeaaf3ba852ca5b32fe4d8a8e9a
Eichik <mar.eikhort@gmail.com>
2016-09-17 12:53:23 +0300

    17

--- js/script1.js
+++ js/script1.js
@@ -1,11 +1,14 @@
 //модуль
-angular.module('vkUserApp', [])
+angular.module('vkTaskApp', [])
 //контроллер
-.controller('vkUserCtrl', ["$scope", "$http", function($scope, $http) {
-    $http.get('http://jsonplaceholder.typicode.com/users').success(function(data) {
-        $scope.users=data;
+.controller('vkTaskCtrl', function($scope, $http) {
+    $scope.recordingVk = function(){
+    $http.get('https:%2F%2Foauth.vk.com%2Fauthorize?client_id=5621033&display=page&redirect_uri=http:%2F%2Flocalhost:8080%2F%2Fcallback&scope=friends&response_type=token&v=5.53&state=123456')
+    .success(function(data) {
+       alert ('Вы успешно прошли регистрацию через ВК')
     });
-}]);
+    };
+});
 
 
 
